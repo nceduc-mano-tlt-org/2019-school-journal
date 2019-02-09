83fb12fdcee3232e1df379d7d5fd1a2b71416667
Quikit <mr.quikit@gmail.com>
2019-02-09 22:20:08 +0300

    For dev

--- src/main/java/ru/nceduc/journal/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.nceduc.journal.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.nceduc.journal.entity.Project;
-
-public interface ProjectRepository extends CrudRepository<Project, String> {
-}
--- src/main/java/ru/nceduc/journal/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.nceduc.journal.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.nceduc.journal.entity.User;
-
-public interface UserRepository extends CrudRepository<User, String> {
-}
