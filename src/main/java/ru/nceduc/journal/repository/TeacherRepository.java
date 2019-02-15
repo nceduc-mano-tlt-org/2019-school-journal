b588a321833b0260f01d0972937862364ea2fd71
Alexey <forhotos@gmail.com>
2019-02-15 13:49:59 +0300

    Created Repo

---
+++ src/main/java/ru/nceduc/journal/repository/TeacherRepository.java
@@ -0,0 +1,9 @@
+package ru.nceduc.journal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.nceduc.journal.entity.Teacher;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, String> {
+}
