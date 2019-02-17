089a7245d40f9a8a0a46ed1017c333cc051b9c2b
Darkuran <wsDarkuran@yandex.ru>
2019-02-17 18:58:31 +0400

    Add project controller

---
+++ src/main/java/ru/nceduc/journal/controller/ProjectController.java
@@ -0,0 +1,28 @@
+package ru.nceduc.journal.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.nceduc.journal.entity.Section;
+import ru.nceduc.journal.service.SectionService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("project/")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class ProjectController {
+
+    private final SectionService sectionService;
+
+    @GetMapping("{id}")
+    public String showProjectPage(Model model) {
+        List<Section> sections = sectionService.getAll();
+        model.addAttribute("sections", sections);
+        return "project";
+    }
+
+}
