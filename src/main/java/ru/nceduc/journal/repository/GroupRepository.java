2c8f19ba1ebf056b992a1c5941e2f4cbf428759c
Darkuran <wsDarkuran@yandex.ru>
2019-02-17 18:54:17 +0400

    Update group/section repository to use for sorting

--- src/main/java/ru/nceduc/journal/repository/GroupRepository.java
+++ src/main/java/ru/nceduc/journal/repository/GroupRepository.java
@@ -1,5 +1,6 @@
 package ru.nceduc.journal.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 import ru.nceduc.journal.entity.Group;
 import ru.nceduc.journal.entity.Section;
@@ -7,7 +8,8 @@ import ru.nceduc.journal.entity.Section;
 import java.util.List;
 
 public interface GroupRepository extends CrudRepository<Group, String> {
-    List<Group> findAll();
-    List<Group> findAllBySection(Section section);
+    List<Group> findAll(Sort sort);
+    List<Group> findAllBySection(Section section, Sort sort);
+    boolean existsBySection(Section section);
 }
 
