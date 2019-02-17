cad15f1c1a52ccd1962c7709f08615182823f00c
Alexey <forhotos@gmail.com>
2019-02-17 21:46:09 +0400

    Updated services

--- src/main/java/ru/nceduc/journal/service/StudentService.java
+++ src/main/java/ru/nceduc/journal/service/StudentService.java
@@ -1,7 +1,6 @@
 package ru.nceduc.journal.service;
 
-import ru.nceduc.journal.dto.StudentDTO;
 import ru.nceduc.journal.entity.Student;
 
-public interface StudentService extends GenericService<Student, StudentDTO> {
+public interface StudentService extends GenericService<Student> {
 }
--- src/main/java/ru/nceduc/journal/service/TeacherService.java
+++ src/main/java/ru/nceduc/journal/service/TeacherService.java
@@ -1,7 +1,6 @@
 package ru.nceduc.journal.service;
 
-import ru.nceduc.journal.dto.TeacherDTO;
 import ru.nceduc.journal.entity.Teacher;
 
-public interface TeacherService extends GenericService<Teacher, TeacherDTO> {
+public interface TeacherService extends GenericService<Teacher> {
 }
