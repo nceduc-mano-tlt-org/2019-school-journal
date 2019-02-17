ef602aa13b25af95b67654bc4239f11ea4039c17
Quikit <mr.quikit@gmail.com>
2019-02-17 21:53:19 +0300

    Added method findByUsername for search for a user by name

--- src/main/java/ru/nceduc/journal/repository/UserRepository.java
+++ src/main/java/ru/nceduc/journal/repository/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import ru.nceduc.journal.entity.User;
 
 public interface UserRepository extends CrudRepository<User, String> {
+    User findByUsername(String username);
 }
