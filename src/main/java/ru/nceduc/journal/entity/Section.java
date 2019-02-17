2e6e963544b73c29071de0475d0d4128a282e57d
Darkuran <wsDarkuran@yandex.ru>
2019-02-17 18:57:05 +0400

    Add a one-to-many association for section entity

--- src/main/java/ru/nceduc/journal/entity/Section.java
+++ src/main/java/ru/nceduc/journal/entity/Section.java
@@ -5,7 +5,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -15,6 +18,8 @@ import javax.persistence.Table;
 public class Section extends AbstractEntity{
     private String name;
     private String description;
+    @OneToMany(mappedBy = "section")
+    private Set<Group> groups = new HashSet<>();
 
     public Section(String name, String description) {
         this.name = name;
