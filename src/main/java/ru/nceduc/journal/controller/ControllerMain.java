693079611fcbcafac735648568f9d345e0962d93
Quikit <mr.quikit@gmail.com>
2019-02-20 22:53:54 +0300

    Change User entity to UersEntity

--- src/main/java/ru/nceduc/journal/controller/ControllerMain.java
+++ src/main/java/ru/nceduc/journal/controller/ControllerMain.java
@@ -1,12 +1,11 @@
 package ru.nceduc.journal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.nceduc.journal.entity.User;
+import ru.nceduc.journal.entity.UserEntity;
 import ru.nceduc.journal.service.impl.UserServiceImpl;
 
 @Controller
@@ -18,7 +17,7 @@ public class ControllerMain {
 
     @GetMapping(value = "/hello")
     public String getHello(Model model){
-        User user = service.getCurrentUsername();
+        UserEntity user = service.getCurrentUsername();
         model.addAttribute("username",user.getUsername());
         model.addAttribute("password",user.getPassword());
         return "hello";
