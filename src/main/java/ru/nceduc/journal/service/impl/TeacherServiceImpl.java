561fb22fb8f186e48f1f919cc654fcd523286aa3
Alexey <forhotos@gmail.com>
2019-02-20 23:12:03 +0400

    Update TeacherServiceImpl.java

--- src/main/java/ru/nceduc/journal/service/impl/TeacherServiceImpl.java
+++ src/main/java/ru/nceduc/journal/service/impl/TeacherServiceImpl.java
@@ -1,46 +1,71 @@
 package ru.nceduc.journal.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import ru.nceduc.journal.dto.TeacherDTO;
 import ru.nceduc.journal.entity.Teacher;
 import ru.nceduc.journal.repository.TeacherRepository;
 import ru.nceduc.journal.service.TeacherService;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class TeacherServiceImpl implements TeacherService {
     private final TeacherRepository teacherRepository;
+    private final ModelMapper modelMapper;
 
     @Override
-    public boolean create(Teacher entity, String parentId) {
-        teacherRepository.save(entity);
-        return true;
+    public TeacherDTO create(TeacherDTO teacherDTO, String parentId) {
+        if (teacherDTO != null) {
+            Teacher teacher = modelMapper.map(teacherDTO, Teacher.class);
+            teacherRepository.save(teacher);
+            return teacherDTO;
+        } else
+            return null;
     }
 
     @Override
-    public boolean delete(String id) {
-        teacherRepository.deleteById(id);
-        return true;
+    public void delete(String id) {
+        if (id != null && teacherRepository.existsById(id))
+            teacherRepository.deleteById(id);
     }
 
     @Override
-    public boolean update(String id, Teacher entity) {
-        entity.setId(id);
-        teacherRepository.save(entity);
-        return true;
+    public TeacherDTO patch(TeacherDTO teacherDTO) {
+        return null;
     }
 
     @Override
-    public Teacher get(String id) {
-        return teacherRepository.getOne(id);
+    public TeacherDTO update(TeacherDTO teacherDTO) {
+        String id = teacherDTO.getId();
+        if (id != null && teacherRepository.existsById(id)){
+            Teacher teacher = modelMapper.map(teacherDTO, Teacher.class);
+            teacher.setModifiedDate(new Date());
+            teacherRepository.save(teacher);
+            return teacherDTO;
+        } else
+            return null;
     }
 
     @Override
-    public List<Teacher> getAll() {
-        return new ArrayList<>(teacherRepository.findAll());
+    public TeacherDTO get(String id) {
+        Teacher teacher = teacherRepository.getOne(id);
+        return modelMapper.map(teacher, TeacherDTO.class);
+    }
+
+    @Override
+    public List<TeacherDTO> getAll() {
+        List<Teacher> teachers = teacherRepository.findAll(Sort.by("createdDate").ascending());
+        List<TeacherDTO> teacherDTOS = new ArrayList<>();
+        for (Teacher element : teachers){
+            teacherDTOS.add(modelMapper.map(element, TeacherDTO.class));
+        }
+        return teacherDTOS;
     }
 }
