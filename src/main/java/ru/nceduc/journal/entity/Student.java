b484d710b4b83768f26720905769a761e675b72c
Alexey <forhotos@gmail.com>
2019-02-20 23:11:53 +0400

    Update Student.java

--- src/main/java/ru/nceduc/journal/entity/Student.java
+++ src/main/java/ru/nceduc/journal/entity/Student.java
@@ -7,7 +7,7 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
 
 @Entity
-@Table (name = "student")
+@Table (name = "students")
 @Getter
 @Setter
 public class Student extends Person {
