c236ae72bb92697d079853c9290c3bb76114f0f2
Alexey <forhotos@gmail.com>
2019-02-20 23:12:24 +0400

    Created Model Mapper Config

---
+++ src/main/java/ru/nceduc/journal/util/ModelMapperConfiguration.java
@@ -0,0 +1,14 @@
+package ru.nceduc.journal.util;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ModelMapperConfiguration {
+    @Bean
+    public ModelMapper modelMapperBean() {
+        return new ModelMapper();
+    }
+}
