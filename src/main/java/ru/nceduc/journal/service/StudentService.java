9b691ff4f688922df3f79368216573242f9f94fc
Alexey <forhotos@gmail.com>
2019-02-20 23:12:06 +0400

    Update StudentService.java

--- src/main/java/ru/nceduc/journal/service/StudentService.java
+++ src/main/java/ru/nceduc/journal/service/StudentService.java
@@ -1,6 +1,6 @@
 package ru.nceduc.journal.service;
 
-import ru.nceduc.journal.entity.Student;
+import ru.nceduc.journal.dto.StudentDTO;
 
-public interface StudentService extends GenericService<Student> {
+public interface StudentService extends GenericService<StudentDTO> {
 }
