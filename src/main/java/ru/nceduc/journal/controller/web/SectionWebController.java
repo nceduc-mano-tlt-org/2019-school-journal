1603011ceb9c0df0c624d93ff346447ceb8a5827
Darkuran <wsDarkuran@yandex.ru>
2019-02-20 22:15:57 +0400

    Update Section web controller to use DTO

--- src/main/java/ru/nceduc/journal/controller/web/SectionWebController.java
+++ src/main/java/ru/nceduc/journal/controller/web/SectionWebController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.nceduc.journal.controller.dto.GroupDTO;
 import ru.nceduc.journal.entity.Group;
 import ru.nceduc.journal.entity.Section;
 import ru.nceduc.journal.service.GroupService;
@@ -24,9 +25,9 @@ public class SectionWebController {
 
     @GetMapping("{id}")
     public String showSectionPage(Model model, @PathVariable String id) {
-        List<Group> groups = groupService.getAllBySectionId(id);
+        List<GroupDTO> groupsDTO = groupService.getAllBySectionId(id);
         Section section = sectionService.get(id);
-        model.addAttribute("groups", groups);
+        model.addAttribute("groups", groupsDTO);
         model.addAttribute("sectionId", id);
         model.addAttribute("sectionName", section.getName());
         return "section";
