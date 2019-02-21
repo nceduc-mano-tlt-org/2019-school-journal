9a2690617469ffc5c96ded84124a7a1770e6d332
Quikit <mr.quikit@gmail.com>
2019-02-21 21:06:32 +0300

    Update check in Project service

--- src/main/java/ru/nceduc/journal/service/impl/ProjectServiceImpl.java
+++ src/main/java/ru/nceduc/journal/service/impl/ProjectServiceImpl.java
@@ -3,7 +3,6 @@ package ru.nceduc.journal.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.nceduc.journal.dto.ProjectDTO;
@@ -12,6 +11,7 @@ import ru.nceduc.journal.repository.ProjectRepository;
 import ru.nceduc.journal.service.GenericService;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -24,35 +24,38 @@ public class ProjectServiceImpl implements GenericService<ProjectDTO> {
 
     @Override
     public ProjectDTO create(ProjectDTO entity) {
-        project = modelMapper.map(entity,Project.class);
-        repository.save(project);
-        if(repository.existsById(entity.getId())){
-            return entity;
-        }
-        return null;
+        if (entity != null) {
+            Project project = modelMapper.map(entity, Project.class);
+            repository.save(project);
+            return modelMapper.map(project, ProjectDTO.class);
+        } else
+            return null;
     }
 
     @Override
     public void delete(String id) {
+        if (id != null && repository.existsById(id))
         repository.deleteById(id);
     }
 
     @Override
     public ProjectDTO patch(ProjectDTO entity) {
-        project.setId(entity.getId());
-        project = repository.getOne(entity.getId());
-        project = modelMapper.map(entity,Project.class);
-        repository.save(project);
-        return entity;
+        String id = entity.getId();
+        if (id != null && repository.existsById(id)) {
+            ProjectDTO mainDTO = new ProjectDTO();
+            modelMapper.map(entity, mainDTO);
+            return getProjectDTO(mainDTO);
+        } else
+            return null;
     }
 
     @Override
     public ProjectDTO update(ProjectDTO entity) {
-        modelMapper.getConfiguration().setSkipNullEnabled(false);
-        project = modelMapper.map(entity,Project.class);
-        repository.save(project);
-        modelMapper.getConfiguration().setSkipNullEnabled(true).setMatchingStrategy(MatchingStrategies.STRICT);
-        return entity;
+        String id = entity.getId();
+        if (id != null && repository.existsById(id)){
+            return getProjectDTO(entity);
+        } else
+            return null;
     }
 
     @Override
@@ -67,10 +70,15 @@ public class ProjectServiceImpl implements GenericService<ProjectDTO> {
         ProjectDTO projectDTO;
         List<ProjectDTO> all = new ArrayList<>();
         for(Project project: repository.findAll()){
-            projectDTO = modelMapper.map(project, ProjectDTO.class);
-            all.add(projectDTO);
+            all.add(modelMapper.map(project, ProjectDTO.class));
         }
         return all;
+    }
+    private ProjectDTO getProjectDTO(ProjectDTO mainDTO) {
+        Project project = modelMapper.map(mainDTO, Project.class);
+        project.setModifiedDate(new Date());
+        repository.save(project);
+        return modelMapper.map(project, ProjectDTO.class);
     }
 }
 
