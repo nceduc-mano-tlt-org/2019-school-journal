981a17b44b890adaa96b157f39d277475612b6f3
Darkuran <wsDarkuran@yandex.ru>
2019-02-21 21:56:20 +0400

    Update SectionWebController to use DTO

--- src/main/java/ru/nceduc/journal/controller/web/SectionWebController.java
+++ src/main/java/ru/nceduc/journal/controller/web/SectionWebController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import ru.nceduc.journal.dto.GroupDTO;
-import ru.nceduc.journal.entity.Section;
+import ru.nceduc.journal.dto.SectionDTO;
 import ru.nceduc.journal.service.GroupService;
 import ru.nceduc.journal.service.SectionService;
 
@@ -20,20 +20,20 @@ public class SectionWebController {
     private final GroupService groupService;
     private final SectionService sectionService;
 
-    @GetMapping("{id}")
-    public String showProjectPage(Model model) {
-        List<Section> sections = sectionService.getAll();
-        model.addAttribute("sections", sections);
+    @GetMapping("sections")
+    public String showAllSectionsPage(Model model) {
+        List<SectionDTO> sectionsDTO = sectionService.getAll();
+        model.addAttribute("sections", sectionsDTO);
         return "sections";
     }
 
     @GetMapping("section/{id}")
     public String showSectionPage(Model model, @PathVariable String id) {
         List<GroupDTO> groupsDTO = groupService.getAllBySectionId(id);
-        Section section = sectionService.get(id);
+        SectionDTO sectionDTO = sectionService.get(id);
         model.addAttribute("groups", groupsDTO);
         model.addAttribute("sectionId", id);
-        model.addAttribute("sectionName", section.getName());
+        model.addAttribute("sectionName", sectionDTO.getName());
         return "section";
     }
 }
