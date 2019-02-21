482e2dca2e35617b171e4c3858aa388701d2d5ea
Darkuran <wsDarkuran@yandex.ru>
2019-02-21 21:07:23 +0400

    Update SectionDTO (fields has been added)

--- src/main/java/ru/nceduc/journal/controller/web/SectionWebController.java
+++ src/main/java/ru/nceduc/journal/controller/web/SectionWebController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import ru.nceduc.journal.dto.GroupDTO;
 import ru.nceduc.journal.entity.Section;
 import ru.nceduc.journal.service.GroupService;
--- src/main/java/ru/nceduc/journal/dto/SectionDTO.java
+++ src/main/java/ru/nceduc/journal/dto/SectionDTO.java
@@ -1,7 +1,15 @@
 package ru.nceduc.journal.dto;
 
 import lombok.Data;
+import ru.nceduc.journal.entity.Group;
+
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 public class SectionDTO {
+    private String id;
+    private String name;
+    private String description;
 }
