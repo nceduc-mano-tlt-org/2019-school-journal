3c0aee9d6958bb02478070c42b76738720bd26f9
Quikit <mr.quikit@gmail.com>
2019-03-04 22:28:11 +0300

    Create method get current project

--- src/main/java/ru/nceduc/journal/service/ProjectService.java
+++ src/main/java/ru/nceduc/journal/service/ProjectService.java
@@ -3,4 +3,5 @@ package ru.nceduc.journal.service;
 import ru.nceduc.journal.dto.ProjectDTO;
 
 public interface ProjectService extends GenericService<ProjectDTO> {
+    ProjectDTO getCurrentProject();
 }
