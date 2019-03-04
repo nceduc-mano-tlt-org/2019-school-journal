ff9b1f041b3d1081d707943e0143ecf6f468bde0
Quikit <mr.quikit@gmail.com>
2019-03-04 20:47:38 +0300

    Add method getAllBy from a specific entity

--- src/main/java/ru/nceduc/journal/service/StudentService.java
+++ src/main/java/ru/nceduc/journal/service/StudentService.java
@@ -2,5 +2,8 @@ package ru.nceduc.journal.service;
 
 import ru.nceduc.journal.dto.StudentDTO;
 
+import java.util.List;
+
 public interface StudentService extends GenericService<StudentDTO> {
+    List<StudentDTO> getAllByGroupId(String groupId);
 }
--- src/main/java/ru/nceduc/journal/service/TeacherService.java
+++ src/main/java/ru/nceduc/journal/service/TeacherService.java
@@ -2,5 +2,8 @@ package ru.nceduc.journal.service;
 
 import ru.nceduc.journal.dto.TeacherDTO;
 
+import java.util.List;
+
 public interface TeacherService extends GenericService<TeacherDTO> {
+    List<TeacherDTO> getAllByGroupId(String groupId);
 }
