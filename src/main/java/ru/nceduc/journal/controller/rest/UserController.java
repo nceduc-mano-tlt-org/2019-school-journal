b40b8dec46ecee5d9a731e6ba3a5ca93aa2df6bd
Darkuran <wsDarkuran@yandex.ru>
2019-03-05 18:26:20 +0400

    Add swagger annotations to user controller

--- src/main/java/ru/nceduc/journal/controller/rest/UserController.java
+++ src/main/java/ru/nceduc/journal/controller/rest/UserController.java
@@ -1,5 +1,7 @@
 package ru.nceduc.journal.controller.rest;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,12 +15,14 @@ import ru.nceduc.journal.service.UserService;
 @RestController
 @RequestMapping("api/v1/user")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Api(description="Users operations", tags = "USER-V1")
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping
+    @ApiOperation(value = "Create a new user")
     @ResponseStatus(HttpStatus.OK)
+    @PostMapping
     public void createUser(UserDTO userDTO) {
         userService.create(userDTO);
     }
