2e65f480ae9bef32c9cd27ea1b3545a82ee17641
Quikit <mr.quikit@gmail.com>
2019-03-06 17:02:17 +0300

    Transferred actions with users to the service.

--- src/main/java/ru/nceduc/journal/controller/rest/RegistrationController.java
+++ src/main/java/ru/nceduc/journal/controller/rest/RegistrationController.java
@@ -8,10 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.nceduc.journal.dto.UserDTO;
-import ru.nceduc.journal.entity.Role;
 import ru.nceduc.journal.service.impl.UserServiceImpl;
-
-import java.util.Collections;
 
 @Controller
 @RequestMapping(value = "/registration")
@@ -28,12 +25,6 @@ public class RegistrationController {
     @ResponseStatus(HttpStatus.OK)
     @PostMapping(value = "")
     public String createUser(UserDTO user){
-        Boolean expected = service.getByName(user.getUsername());
-        if (expected == true){
-            return "registration";
-        }
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
         service.create(user);
         return "login";
     }
