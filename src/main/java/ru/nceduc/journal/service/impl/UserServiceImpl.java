cd23223f0074c1628f4a589dfc82a83c03409d7b
Quikit <mr.quikit@gmail.com>
2019-03-06 16:46:41 +0300

    Implementation method find all by the project, returting users and change update and patch methods

--- src/main/java/ru/nceduc/journal/service/impl/UserServiceImpl.java
+++ src/main/java/ru/nceduc/journal/service/impl/UserServiceImpl.java
@@ -3,17 +3,20 @@ package ru.nceduc.journal.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 import ru.nceduc.journal.dto.UserDTO;
 import ru.nceduc.journal.entity.Project;
+import ru.nceduc.journal.entity.Role;
 import ru.nceduc.journal.entity.UserEntity;
 import ru.nceduc.journal.repository.ProjectRepository;
 import ru.nceduc.journal.repository.UserRepository;
 import ru.nceduc.journal.service.UserService;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -23,10 +26,7 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository repositoryUser;
     private final ProjectRepository repositoryProject;
-    
-    @Autowired
-    private ModelMapper modelMapper;
-    private UserEntity userEntity = null;
+    private final ModelMapper modelMapper;
 
     @Override
     public UserEntity getCurrentUsername() {
@@ -40,11 +40,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDTO create(UserDTO entity) {
-        if (entity != null) {
+        if (entity != null && !repositoryUser.existsByUsername(entity.getUsername())) {
             Project project = new Project();
             Project projectInDB = repositoryProject.save(project);
             UserEntity userEntity = modelMapper.map(entity, UserEntity.class);
             userEntity.setProject(projectInDB);
+            userEntity.setActive(true);
+            userEntity.setRoles(Collections.singleton(Role.USER));
             repositoryUser.save(userEntity);
             return modelMapper.map(userEntity, UserDTO.class);
         } else
@@ -60,54 +62,58 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDTO patch(UserDTO entity) {
         String id = entity.getId();
-        if (id != null && repositoryUser.existsById(id)) {
-            UserDTO mainDTO = new UserDTO();
-            modelMapper.map(entity, mainDTO);
-            return getUserDTO(mainDTO);
-        } else
-            return null;
+        UserDTO userDTO = this.get(id);
+        modelMapper.map(entity, userDTO);
+        return update(userDTO);
     }
 
     @Override
     public UserDTO update(UserDTO entity) {
         String id = entity.getId();
-        if (id != null && repositoryUser.existsById(id)){
-            return getUserDTO(entity);
-        } else
+        if (id != null && repositoryUser.existsById(id)) {
+            UserEntity userEntity = modelMapper.map(entity, UserEntity.class);
+            userEntity.setCreatedDate(repositoryUser.findById(id).get().getCreatedDate());
+            userEntity.setModifiedDate(new Date());
+            repositoryUser.save(userEntity);
+            return entity;
+        } else {
             return null;
+        }
     }
 
     @Override
     public UserDTO get(String id) {
-        userEntity = repositoryUser.getOne(id);
+        UserEntity userEntity = repositoryUser.getOne(id);
         UserDTO userDTO = modelMapper.map(userEntity,UserDTO.class);
         return userDTO;
     }
 
     @Override
     public List<UserDTO> getAll() {
-        UserDTO projectDTO;
         List<UserDTO> all = new ArrayList<>();
-        for(UserEntity user: repositoryUser.findAll()){
+        repositoryUser.findAll(Sort.by("createdDate").ascending()).forEach(user -> {
             all.add(modelMapper.map(user, UserDTO.class));
-        }
+        });
         return all;
-    }
-
-    private UserDTO getUserDTO(UserDTO mainDTO) {
-        UserEntity userEntity = modelMapper.map(mainDTO, UserEntity.class);
-        userEntity.setModifiedDate(new Date());
-        repositoryUser.save(userEntity);
-        return modelMapper.map(userEntity, UserDTO.class);
     }
 
     @Override
     public boolean getByName(String name){
-        userEntity = repositoryUser.findByUsername(name);
+        UserEntity userEntity = repositoryUser.findByUsername(name);
         if (userEntity == null){
             return false;
         }
         return true;
+    }
+
+    @Override
+    public List<UserDTO> findAllByProject(String projectId) {
+        Project project = repositoryProject.findById(projectId).get();
+        List<UserDTO> userDTO = new ArrayList<>();
+        repositoryUser.findAllByProject(project, Sort.by("createdDate").ascending()).forEach(user -> {
+            userDTO.add(modelMapper.map(user, UserDTO.class));
+        });
+        return userDTO;
     }
 }
 
