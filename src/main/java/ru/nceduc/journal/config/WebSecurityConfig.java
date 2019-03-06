74ffbddce0b85bf3f4fbc78919586752e9e13e08
Quikit <mr.quikit@gmail.com>
2019-03-06 17:04:24 +0300

    Adding lombok annotation for initializing private final places

--- src/main/java/ru/nceduc/journal/config/WebSecurityConfig.java
+++ src/main/java/ru/nceduc/journal/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.nceduc.journal.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,9 +13,11 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private DataSource dataSource;
+
+    private final DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
