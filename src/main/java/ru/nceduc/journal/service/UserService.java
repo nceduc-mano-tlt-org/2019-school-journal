388503c2b048b683aa35bb637d2f0959544a09f5
Quikit <mr.quikit@gmail.com>
2019-03-06 16:43:57 +0300

    Add metchod returning users by the project

--- src/main/java/ru/nceduc/journal/service/UserService.java
+++ src/main/java/ru/nceduc/journal/service/UserService.java
@@ -3,7 +3,10 @@ package ru.nceduc.journal.service;
 import ru.nceduc.journal.dto.UserDTO;
 import ru.nceduc.journal.entity.UserEntity;
 
+import java.util.List;
+
 public interface UserService extends GenericService<UserDTO> {
+    List<UserDTO> findAllByProject(String projectId);
     UserEntity getCurrentUsername();
     boolean getByName(String name);
 }
