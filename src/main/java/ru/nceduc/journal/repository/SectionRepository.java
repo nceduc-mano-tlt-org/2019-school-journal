e66a7339e142413b4c3c4e34875bc367b63b7aac
Quikit <mr.quikit@gmail.com>
2019-03-06 17:00:21 +0300

    Adding method findAllByGroup for will returning all sections tied the project.

--- src/main/java/ru/nceduc/journal/repository/SectionRepository.java
+++ src/main/java/ru/nceduc/journal/repository/SectionRepository.java
@@ -2,10 +2,12 @@ package ru.nceduc.journal.repository;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.nceduc.journal.entity.Project;
 import ru.nceduc.journal.entity.Section;
 
 import java.util.List;
 
 public interface SectionRepository extends JpaRepository<Section, String> {
+    List<Section> findAllByProject(Project project, Sort sort);
     List<Section> findAll(Sort sort);
 }
