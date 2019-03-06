b4c312d07e7551176af8be0789f99bcdcea8cf52
Quikit <mr.quikit@gmail.com>
2019-03-06 16:59:54 +0300

    Adding method findAllByGroup for will returning all students tied the group.

--- src/main/java/ru/nceduc/journal/repository/StudentRepository.java
+++ src/main/java/ru/nceduc/journal/repository/StudentRepository.java
@@ -1,7 +1,12 @@
 package ru.nceduc.journal.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.nceduc.journal.entity.Group;
 import ru.nceduc.journal.entity.Student;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, String> {
+    List<Student> findAllByGroup(Group group, Sort sort);
 }
