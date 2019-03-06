b4d164f08606fa6bb47768b083d48391cd8c7104
Quikit <mr.quikit@gmail.com>
2019-03-06 16:58:42 +0300

    Adding method findAllByProject for will returning all users tied the project. And create existsByUsername for chek Is there such a user in DB.

--- src/main/java/ru/nceduc/journal/repository/UserRepository.java
+++ src/main/java/ru/nceduc/journal/repository/UserRepository.java
@@ -1,8 +1,15 @@
 package ru.nceduc.journal.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.nceduc.journal.entity.Project;
 import ru.nceduc.journal.entity.UserEntity;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<UserEntity, String> {
+    List<UserEntity> findAllByProject(Project project, Sort sort);
     UserEntity findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
