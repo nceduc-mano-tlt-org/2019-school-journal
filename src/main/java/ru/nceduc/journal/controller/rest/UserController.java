0006fa5d266eac2306dc79e25b7449ddb267612d
Darkuran <wsDarkuran@yandex.ru>
2019-03-10 13:54:21 +0400

    Add method to authenticate user

--- src/main/java/ru/nceduc/journal/controller/rest/UserController.java
+++ src/main/java/ru/nceduc/journal/controller/rest/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.nceduc.journal.dto.UserDTO;
-import ru.nceduc.journal.entity.UserEntity;
+import ru.nceduc.journal.service.AuthService;
 import ru.nceduc.journal.service.UserService;
 
 import java.util.List;
@@ -20,6 +20,7 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final AuthService authService;
 
     @ApiOperation(value = "Get all users") // TODO: secure this
     @GetMapping("/")
@@ -29,8 +30,14 @@ public class UserController {
 
     @ApiOperation(value = "Create a new user")
     @ResponseStatus(HttpStatus.OK)
-    @PostMapping("/signup")
+    @PostMapping("/signup/")
     public void createUser(@RequestBody UserDTO userDTO) {
         userService.create(userDTO);
+    }
+
+    @ApiOperation(value = "Authorize a new user")
+    @PostMapping("/signin/")
+    public void authenticate(@RequestBody UserDTO userDTO) {
+        authService.authUser(userDTO);
     }
 }
