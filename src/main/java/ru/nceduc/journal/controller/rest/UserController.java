72c21922168529f82b2d23d8eb3dcd03f8cdc880
Quikit <mr.quikit@gmail.com>
2019-03-13 22:20:21 +0300

    Removed unnecessary

--- src/main/java/ru/nceduc/journal/controller/rest/UserController.java
+++ src/main/java/ru/nceduc/journal/controller/rest/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.nceduc.journal.dto.UserDTO;
-import ru.nceduc.journal.entity.UserEntity;
 import ru.nceduc.journal.service.UserService;
 
 import java.util.List;
