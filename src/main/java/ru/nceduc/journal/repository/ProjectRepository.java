92aafd7ed05c762a711704f4f9b24a8431bbce38
Alexey <forhotos@gmail.com>
2019-03-15 01:25:50 +0400

    Add 2nd parameter

--- src/main/java/ru/nceduc/journal/repository/ProjectRepository.java
+++ src/main/java/ru/nceduc/journal/repository/ProjectRepository.java
@@ -1,11 +1,13 @@
 package ru.nceduc.journal.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.nceduc.journal.entity.Project;
 import ru.nceduc.journal.entity.UserEntity;
 
+
 import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, String> {
-    List<Project> findAllByUser(UserEntity user);
+    List<Project> findAllByUser(UserEntity user, Sort sort);
 }
