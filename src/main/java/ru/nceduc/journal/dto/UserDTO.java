3909904c9ec9ca96698994fb4cd6f0d9a266570a
Darkuran <wsDarkuran@yandex.ru>
2019-03-16 18:35:09 +0400

    Add readOnly property to id/projectId fields

--- src/main/java/ru/nceduc/journal/dto/UserDTO.java
+++ src/main/java/ru/nceduc/journal/dto/UserDTO.java
@@ -1,14 +1,14 @@
 package ru.nceduc.journal.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import ru.nceduc.journal.entity.Role;
-
-import java.util.Set;
 
 @Data
 public class UserDTO {
+    @ApiModelProperty(readOnly = true)
     private String id;
     private String username;
     private String password;
+    @ApiModelProperty(readOnly = true)
     private String projectId;
 }
