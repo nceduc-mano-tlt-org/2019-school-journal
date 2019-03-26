fb496eefe460070b9db291c72af035245ce1df8f
Darkuran <wsDarkuran@yandex.ru>
2019-03-26 14:53:38 +0400

    Add some new fields to group/student dto's

--- src/main/java/ru/nceduc/journal/dto/GroupDTO.java
+++ src/main/java/ru/nceduc/journal/dto/GroupDTO.java
@@ -1,11 +1,17 @@
 package ru.nceduc.journal.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class GroupDTO {
     private String id;
     private String name;
     private String description;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date startDate;
+    private long cost;
     private String sectionId;
 }
