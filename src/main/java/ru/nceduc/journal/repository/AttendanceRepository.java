78c44bf77a3f74797afabbafb410d3e85edd9992
Quikit <mr.quikit@gmail.com>
2019-03-28 20:47:37 +0300

    Correction

--- src/main/java/ru/nceduc/journal/repository/AttendanceRepository.java
+++ src/main/java/ru/nceduc/journal/repository/AttendanceRepository.java
@@ -2,14 +2,14 @@ package ru.nceduc.journal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.nceduc.journal.entity.Attendance;
-import ru.nceduc.journal.entity.Student;
 
 import java.util.Date;
 import java.util.List;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, String> {
-    //List<Attendance> findAllByDateVisitBetween(Date startDate, Date endDate);
     List<Attendance> findAllByGroup_IdAndStudent_IdAndDateVisitBetween(String groupId, String studentId, Date startDate, Date endDate);
-    boolean existsByGroup_IdAndStudent_Id(String group_id, String Student_id);
-    void deleteByGroup_IdAndStudent_Id(String group_id, String Student_id);
+
+    boolean existsByGroup_IdAndStudent_IdAndDateVisit(String group_id, String Student_id, Date date);
+
+    void deleteByGroup_IdAndStudent_IdAndDateVisit(String group_id, String Student_id, Date date);
 }
