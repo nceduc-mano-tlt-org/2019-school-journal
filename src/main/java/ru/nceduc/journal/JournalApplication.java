b6ec6b0d8eb8f0c23b58cdf974526d5ccc39f987
Quikit <mr.quikit@gmail.com>
2019-03-28 20:50:08 +0300

    Correction

--- src/main/java/ru/nceduc/journal/JournalApplication.java
+++ src/main/java/ru/nceduc/journal/JournalApplication.java
@@ -3,12 +3,6 @@ package ru.nceduc.journal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
-
 @SpringBootApplication
 public class JournalApplication {
 
--- src/main/java/ru/nceduc/journal/service/AttendanceService.java
+++ src/main/java/ru/nceduc/journal/service/AttendanceService.java
@@ -1,17 +1,18 @@
 package ru.nceduc.journal.service;
 
-import ru.nceduc.journal.dto.AttendanceDTO;
+import ru.nceduc.journal.dto.AttendanceGroupDTO;
+import ru.nceduc.journal.dto.AttendanceStudentDTO;
 import ru.nceduc.journal.dto.StudentDTO;
-import ru.nceduc.journal.entity.Student;
 
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 public interface AttendanceService {
-    Map<StudentDTO, List<Date>> getAttendanceByGroup(String groupId, Date date);
-    Optional<AttendanceDTO> create(AttendanceDTO attendanceDTO);
-    void delete(String groupId, String studentId);
+    Map<StudentDTO, List<Date>> getAttendanceByGroup(AttendanceGroupDTO attendanceGroupDTO);
+
+    Optional<AttendanceStudentDTO> create(AttendanceStudentDTO attendanceStudentDTO);
+
+    void delete(AttendanceStudentDTO attendanceStudentDTO);
 }
