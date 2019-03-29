fb75273a59fde4ab1328d5136769e175a597083b
Quikit <mr.quikit@gmail.com>
2019-03-29 20:19:55 +0300

    Was added @DateTimeFormat for date place

--- src/main/java/ru/nceduc/journal/dto/AttendanceStudentDTO.java
+++ src/main/java/ru/nceduc/journal/dto/AttendanceStudentDTO.java
@@ -1,13 +1,17 @@
 package ru.nceduc.journal.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Data
 public class AttendanceStudentDTO {
+    @ApiModelProperty(readOnly = true)
     private String id;
     private String studentId;
     private String groupId;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateVisit;
 }
