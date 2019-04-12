01f9c7f981788029e332aa0672eecfd1ccd158f6
Darkuran <wsDarkuran@yandex.ru>
2019-04-12 20:59:19 +0400

    Implement setUp method in student controller tests

--- src/main/java/ru/nceduc/journal/dto/StudentDTO.java
+++ src/main/java/ru/nceduc/journal/dto/StudentDTO.java
@@ -1,11 +1,15 @@
 package ru.nceduc.journal.dto;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentDTO {
     private String id;
     private String firstName;
--- src/test/java/ru/nceduc/journal/controller/StudentControllerTests.java
+++ src/test/java/ru/nceduc/journal/controller/StudentControllerTests.java
@@ -11,7 +11,13 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.nceduc.journal.controller.rest.StudentController;
+import ru.nceduc.journal.dto.GroupDTO;
+import ru.nceduc.journal.dto.StudentDTO;
 import ru.nceduc.journal.service.StudentService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(StudentController.class)
@@ -27,9 +33,30 @@ public class StudentControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
+    private GroupDTO groupDTO;
+    private StudentDTO firstStudent;
+    private StudentDTO secondStudent;
+    private StudentDTO thirdStudent;
+    private List<StudentDTO> allStudents;
+    private List<StudentDTO> studentsByGroup;
 
     @Before
     public void setUp() {
+        String groupId = UUID.randomUUID().toString();
+
+        groupDTO = new GroupDTO(groupId, "First group", "Desc for first group", null, 1000, UUID.randomUUID().toString());
+        firstStudent = new StudentDTO(UUID.randomUUID().toString(), "Ivan", "Ivanov", groupId, null);
+        secondStudent = new StudentDTO(UUID.randomUUID().toString(), "Petr", "Petrov", groupId, null);
+        thirdStudent = new StudentDTO(UUID.randomUUID().toString(), "Alexey", "Frolov", UUID.randomUUID().toString(), null);
+
+        allStudents = new ArrayList<>();
+        allStudents.add(firstStudent);
+        allStudents.add(secondStudent);
+        allStudents.add(thirdStudent);
+
+        studentsByGroup = new ArrayList<>();
+        studentsByGroup.add(firstStudent);
+        studentsByGroup.add(secondStudent);
     }
 
     @Test
