cdff44b92269d5beb4674a3aa8bd76166cf5e1d2
Darkuran <wsDarkuran@yandex.ru>
2019-04-12 20:15:29 +0400

    Add test for getGroup method

--- src/test/java/ru/nceduc/journal/controller/GroupControllerTests.java
+++ src/test/java/ru/nceduc/journal/controller/GroupControllerTests.java
@@ -3,9 +3,11 @@ package ru.nceduc.journal.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -15,10 +17,15 @@ import ru.nceduc.journal.dto.GroupDTO;
 import ru.nceduc.journal.dto.SectionDTO;
 import ru.nceduc.journal.service.GroupService;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static sun.plugin2.util.PojoUtil.toJson;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(GroupController.class)
@@ -47,9 +54,9 @@ public class GroupControllerTests {
         String sectionId = UUID.randomUUID().toString();
         
         sectionDTO = new SectionDTO(sectionId, "English", "En description", UUID.randomUUID().toString());
-        firstGroup = new GroupDTO(UUID.randomUUID().toString(), "First group", "Desc for first group", new Date(), 1000, sectionId);
-        secondGroup = new GroupDTO(UUID.randomUUID().toString(), "Second group", "Desc for second group", new Date(), 2000, sectionId);
-        thirdGroup = new GroupDTO(UUID.randomUUID().toString(), "Third group", "Desc for third group", new Date(), 3000, UUID.randomUUID().toString());
+        firstGroup = new GroupDTO(UUID.randomUUID().toString(), "First group", "Desc for first group", null, 1000, sectionId);
+        secondGroup = new GroupDTO(UUID.randomUUID().toString(), "Second group", "Desc for second group",null, 2000, sectionId);
+        thirdGroup = new GroupDTO(UUID.randomUUID().toString(), "Third group", "Desc for third group", null, 3000, UUID.randomUUID().toString());
 
         allGroups = new ArrayList<>();
         allGroups.add(firstGroup);
@@ -62,7 +69,17 @@ public class GroupControllerTests {
     }
 
     @Test
-    public void getGroup() {
+    public void getGroup() throws Exception {
+        String id = firstGroup.getId();
+        Mockito.when(groupService.get(id)).thenReturn(Optional.of(firstGroup));
+
+        mockMvc.perform(get(mapping + "/" + id))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().json(toJson(firstGroup)));
+
+        mockMvc.perform(get(mapping + "/" + "invalidId"))
+                .andExpect(status().isNotFound());
     }
 
     @Test
