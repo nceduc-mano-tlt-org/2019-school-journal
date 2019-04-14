e5f5ba62ef77a8d221a7dcd3748b82e038d2fa0a
Alexey <forhotos@gmail.com>
2019-04-14 18:57:46 +0400

    Update AttendanceStudentService.java

--- src/main/java/ru/nceduc/journal/service/AttendanceStudentService.java
+++ src/main/java/ru/nceduc/journal/service/AttendanceStudentService.java
@@ -1,11 +1,14 @@
 package ru.nceduc.journal.service;
 
+import ru.nceduc.journal.dto.AttendanceGroupDTO;
 import ru.nceduc.journal.dto.AttendanceStudentDTO;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface AttendanceStudentService{
+    List<AttendanceStudentDTO> getAllByGroupDTO(AttendanceGroupDTO attendanceGroupDTO);
+
     List<AttendanceStudentDTO> getAllByGroupId(String id);
 
     List<AttendanceStudentDTO> getAll();
