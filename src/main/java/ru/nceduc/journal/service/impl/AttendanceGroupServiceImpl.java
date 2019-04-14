6399d996241fc4dbc6bfeea854a1e9c7f49ccbaa
Alexey <forhotos@gmail.com>
2019-04-14 18:57:33 +0400

    Update AttendanceGroupServiceImpl.java

--- src/main/java/ru/nceduc/journal/service/impl/AttendanceGroupServiceImpl.java
+++ src/main/java/ru/nceduc/journal/service/impl/AttendanceGroupServiceImpl.java
@@ -3,11 +3,14 @@ package ru.nceduc.journal.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import ru.nceduc.journal.dto.AttendanceGroupDTO;
+import ru.nceduc.journal.entity.AttendanceGroup;
 import ru.nceduc.journal.repository.AttendanceGroupRepository;
 import ru.nceduc.journal.service.AttendanceGroupService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,34 +20,45 @@ public class AttendanceGroupServiceImpl implements AttendanceGroupService {
 
     private final AttendanceGroupRepository attendanceRepository;
     private final ModelMapper modelMapper;
-
-    @Override
-    public List<AttendanceGroupDTO> getAllByGroupDTO(AttendanceGroupDTO attendanceGroupDTO) {
-        return null;
-    }
-
+    
     @Override
     public List<AttendanceGroupDTO> getAllByGroupId(String groupId) {
-        return null;
+        List<AttendanceGroupDTO> attendanceGroupDTOS = new ArrayList<>();
+        attendanceRepository.findAllByGroupId(groupId).forEach(attendance ->
+                attendanceGroupDTOS.add(modelMapper.map(attendance, AttendanceGroupDTO.class)));
+        return attendanceGroupDTOS;
     }
 
     @Override
     public Optional<AttendanceGroupDTO> get(String id) {
-        return Optional.empty();
+        return attendanceRepository.findById(id).map(attendance ->
+                modelMapper.map(attendance, AttendanceGroupDTO.class));
     }
 
     @Override
     public List<AttendanceGroupDTO> getAll() {
-        return null;
+        List<AttendanceGroupDTO> attendancesDTO = new ArrayList<>();
+        attendanceRepository.findAll(Sort.by("createdDate").ascending()).forEach(attendance -> {
+            attendancesDTO.add(modelMapper.map(attendance, AttendanceGroupDTO.class));
+        });
+        return attendancesDTO;
     }
 
     @Override
     public Optional<AttendanceGroupDTO> create(AttendanceGroupDTO entity) {
+        Optional<AttendanceGroupDTO> optionalDTO = Optional.ofNullable(entity);
+        if (optionalDTO.isPresent()) {
+            AttendanceGroup attendance =
+                    attendanceRepository.save(modelMapper.map(optionalDTO.get(), AttendanceGroup.class));
+            return Optional.of(modelMapper.map(attendance, AttendanceGroupDTO.class));
+        }
         return Optional.empty();
     }
 
     @Override
     public void delete(String id) {
-
+        if (id != null && attendanceRepository.existsById(id)){
+            attendanceRepository.deleteById(id);
+        }
     }
 }
