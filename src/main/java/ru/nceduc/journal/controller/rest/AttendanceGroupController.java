c5c62fc56791a9c5090c24136dcf885c48475de7
Shelbycobra196837 <shelbycobra196837123@gmail.com>
2019-04-14 20:38:08 +0300

    Controller AttendanceGroup and AttendanceStudent

--- src/main/java/ru/nceduc/journal/controller/rest/AttendanceGroupController.java
+++ src/main/java/ru/nceduc/journal/controller/rest/AttendanceGroupController.java
@@ -1,11 +1,20 @@
 package ru.nceduc.journal.controller.rest;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import ru.nceduc.journal.dto.AttendanceGroupDTO;
+import ru.nceduc.journal.dto.GroupDTO;
+import ru.nceduc.journal.entity.AttendanceGroup;
 import ru.nceduc.journal.service.AttendanceGroupService;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/group/attendance")
@@ -13,4 +22,44 @@ import ru.nceduc.journal.service.AttendanceGroupService;
 @Api(description = "Operations pertaining to attendance of groups in School Journal", tags = "ATTENDANCE-GROUP-V1")
 public class AttendanceGroupController {
     private final AttendanceGroupService attendanceService;
+
+
+    @ApiOperation(value = "Delete a attendance")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<AttendanceGroupDTO> deleteAttendance(@PathVariable String id) {
+        attendanceService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @ApiOperation(value = "Create a new attendance")
+    @PostMapping("/")
+    public ResponseEntity<AttendanceGroupDTO> createAttendance(@RequestBody AttendanceGroupDTO attendanceGroupDTO) {
+        Optional<AttendanceGroupDTO> optionalDTO = attendanceService.create(attendanceGroupDTO);
+        return optionalDTO.map(dto -> new ResponseEntity<>(dto, HttpStatus.CREATED))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.BAD_REQUEST));
+    }
+
+    @ApiOperation(value = "Get all attendance")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("/")
+    public ResponseEntity<List<AttendanceGroupDTO>> getAllAttendance() {
+        return new ResponseEntity<>(attendanceService.getAll(), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Get details of specific attendance")
+    @GetMapping("/{id}")
+    public ResponseEntity<AttendanceGroupDTO> getAttendance(@PathVariable String id) {
+        Optional<AttendanceGroupDTO> optionalDTO = attendanceService.get(id);
+        return optionalDTO.map(dto -> new ResponseEntity<>(dto, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @ApiOperation(value = "Get all groups by id")
+    @GetMapping("/by-group/{groupId}")
+    public ResponseEntity<List<AttendanceGroupDTO>> getAllByAttendanceId(@PathVariable String groupId) {
+        return new ResponseEntity<>(attendanceService.getAllByGroupId(groupId), HttpStatus.OK);
+    }
+
+
+
 }
--- src/main/java/ru/nceduc/journal/controller/rest/AttendanceStudentController.java
+++ src/main/java/ru/nceduc/journal/controller/rest/AttendanceStudentController.java
@@ -1,14 +1,61 @@
 package ru.nceduc.journal.controller.rest;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.nceduc.journal.dto.AttendanceGroupDTO;
+import ru.nceduc.journal.dto.AttendanceStudentDTO;
+import ru.nceduc.journal.dto.GroupDTO;
+import ru.nceduc.journal.dto.StudentDTO;
+import ru.nceduc.journal.service.AttendanceStudentService;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/student/attendance")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Api(description = "Operations pertaining to attendance of students in School Journal", tags = "ATTENDANCE-STUDENT-V1")
 public class AttendanceStudentController {
+    private final AttendanceStudentService attendanceStudentService;
+
+    @ApiOperation(value = "Get all student by id")
+    @GetMapping("/by-group/{studentId}")
+    public ResponseEntity<List<AttendanceStudentDTO>> getAllByAttendanceStudentId(@PathVariable String studentId) {
+        return new ResponseEntity<>(attendanceStudentService.getAllByGroupId(studentId), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Delete a attendanceStudent")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<AttendanceStudentDTO> deleteAttendanceStudent(@PathVariable String id) {
+        attendanceStudentService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @ApiOperation(value = "Create a new attendanceStudent")
+    @PostMapping("/")
+    public ResponseEntity<AttendanceStudentDTO> createAttendanceStudent(@RequestBody AttendanceStudentDTO attendanceStudentDTO) {
+        Optional<AttendanceStudentDTO> optionalDTO = attendanceStudentService.create(attendanceStudentDTO);
+        return optionalDTO.map(dto -> new ResponseEntity<>(dto, HttpStatus.CREATED))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.BAD_REQUEST));
+    }
+
+    @ApiOperation(value = "Get details of specific AttendanceStudent")
+    @GetMapping("/{id}")
+    public ResponseEntity<AttendanceStudentDTO> getAttendanceStudent(@PathVariable String id) {
+        Optional<AttendanceStudentDTO> optionalDTO = attendanceStudentService.get(id);
+        return optionalDTO.map(dto -> new ResponseEntity<>(dto, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @ApiOperation(value = "Get all group by ID of attendance")
+    @GetMapping("/by-group/{id}")
+    public ResponseEntity<List<AttendanceGroupDTO>> getAllByGroupDTO(@PathVariable String id) {
+        return new ResponseBody<>(attendanceStudentService.getAllByGroupDTO(), HttpStatus.OK);
+    }
+
 }
