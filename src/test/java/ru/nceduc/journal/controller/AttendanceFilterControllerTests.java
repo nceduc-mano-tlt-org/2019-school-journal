bcf1fe5b6ffa4e3dd61165dae2d600493cd72b02
Darkuran <wsDarkuran@yandex.ru>
2019-04-21 17:06:26 +0400

    Add tests for get methods

--- src/test/java/ru/nceduc/journal/controller/AttendanceFilterControllerTests.java
+++ src/test/java/ru/nceduc/journal/controller/AttendanceFilterControllerTests.java
@@ -3,24 +3,28 @@ package ru.nceduc.journal.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.nceduc.journal.controller.rest.AttendanceFilterController;
-import ru.nceduc.journal.controller.rest.AttendanceStudentController;
 import ru.nceduc.journal.dto.AttendanceFilterDTO;
 import ru.nceduc.journal.service.AttendanceFilterService;
-import ru.nceduc.journal.service.AttendanceStudentService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static sun.plugin2.util.PojoUtil.toJson;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(AttendanceFilterController.class)
@@ -37,6 +41,7 @@ public class AttendanceFilterControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
+    private String groupId;
     private AttendanceFilterDTO firstFilter;
     private AttendanceFilterDTO secondFilter;
     private AttendanceFilterDTO thirdFilter;
@@ -44,8 +49,8 @@ public class AttendanceFilterControllerTests {
     private List<AttendanceFilterDTO> filtersByGroup;
 
     @Before
-    public void setUp() throws Exception {
-        String groupId = UUID.randomUUID().toString();
+    public void setUp() {
+        groupId = UUID.randomUUID().toString();
         firstFilter = new AttendanceFilterDTO(UUID.randomUUID().toString(), groupId, 10, 2020);
         secondFilter = new AttendanceFilterDTO(UUID.randomUUID().toString(), groupId, 11, 2020);
         thirdFilter = new AttendanceFilterDTO(UUID.randomUUID().toString(), groupId, 11, 2020);
@@ -61,20 +66,49 @@ public class AttendanceFilterControllerTests {
     }
 
     @Test
-    public void getFilter() {
+    public void getFilter() throws Exception {
+        String id = firstFilter.getId();
+        Mockito.when(attendanceFilterService.get(id)).thenReturn(Optional.of(firstFilter));
+
+        mockMvc.perform(get(mapping + "/" + id))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().json(toJson(firstFilter)));
+
+        mockMvc.perform(get(mapping + "/" + "invalidId"))
+                .andExpect(status().isNotFound());
     }
 
     @Test
-    public void getAllFilters() {
+    public void getAllFilters() throws Exception {
+        mockMvc.perform(get(mapping + "/"))
+                .andExpect(status().isForbidden());
     }
 
     @WithMockUser(authorities = "ADMIN")
     @Test
-    public void getAllFiltersByAdmin() {
+    public void getAllFiltersByAdmin() throws Exception {
+        Mockito.when(attendanceFilterService.getAll()).thenReturn(allFilters);
+
+        mockMvc.perform(get(mapping + "/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().json(toJson(allFilters.toArray())));
     }
 
     @Test
-    public void getAllAttendancesByGroupId() {
+    public void getAllFiltersByGroupId() throws Exception {
+        Mockito.when(attendanceFilterService.getAllByGroupId(groupId)).thenReturn(filtersByGroup);
+
+        mockMvc.perform(get(mapping + "/by-group/" + groupId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().json(toJson(filtersByGroup.toArray())));
+
+        mockMvc.perform(get(mapping + "/by-group/" + "invalidId"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().string("[]"));
     }
 
     @Test
